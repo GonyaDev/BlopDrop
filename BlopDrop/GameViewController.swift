004a18ae9683e1a87859387272cb6e3da2f7f2c3
GonyaDev <gonyadev@yandex.ru>
2022-10-22 23:23:51 +0300

    initial commit

--- BlopDrop/GameViewController.swift
+++ BlopDrop/GameViewController.swift
@@ -13,6 +13,7 @@ class GameViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        //Lets start
         
         if let view = self.view as! SKView? {
             // Load the SKScene from 'GameScene.sks'
