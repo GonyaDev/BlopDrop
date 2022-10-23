11cfc2ead280c22be583354341fcecda81eae4bc
GonyaDev <gonyadev@yandex.ru>
2022-10-23 09:34:21 +0300

    Создание вью и сцены

--- BlopDrop/GameViewController.swift
+++ BlopDrop/GameViewController.swift
@@ -12,6 +12,31 @@ import GameplayKit
 class GameViewController: UIViewController {
 
     override func viewDidLoad() {
+        
+        //Создание Вью
+        if let view = self.view as! SKView? {
+            
+            //Создание сцены
+            let scene = GameScene(size: view.bounds.size)
+            
+            //Настройка отображения сцены
+            scene.scaleMode = .aspectFill
+            
+            //Настройка бэкграунда
+            scene.backgroundColor = UIColor(red: 105/255,
+                                            green: 157/255,
+                                            blue: 181/255,
+                                            alpha: 1.0)
+            //Отображение сцены
+            view.presentScene(scene)
+            
+            //Настройка опций
+            view.ignoresSiblingOrder = false
+            view.showsPhysics = false
+            view.showsFPS = true
+            view.showsNodeCount = true
+        }
+        
         super.viewDidLoad()
     }
 
